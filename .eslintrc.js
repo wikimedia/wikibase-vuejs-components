24c84e9112bd7b46a82134af01244b9140049db4
bitPogo <matthias.geisler@wikimedia.de>
2019-10-29 20:28:26 +0100

    Add storybook
    This patch adds the storybook for the component lib.
    
    Bug: T240329
    Change-Id: Ib930e5d5d00f00b679add0ad94b05d5d4c1a3c7f

--- .eslintrc.js
+++ .eslintrc.js
@@ -108,7 +108,7 @@ module.exports = {
 			},
 		},
 		{
-			files: [ '**/*.js' ],
+			files: [ '*.js' ],
 			rules: {
 				'@typescript-eslint/explicit-function-return-type': 'off',
 				'@typescript-eslint/explicit-member-accessibility': 'off',
