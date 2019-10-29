79d66375219aa9f48c15eba2d8c6509f43c5aec4
bitPogo <matthias.geisler@wikimedia.de>
2019-10-29 20:28:26 +0100

    shared-vue: alter package name
    This patch simply align the name of the package with vuex-helpers.
    
    Bug: T240329
    Change-Id: I7b1c878a9f5b2bf8a31f2510a9b28b73a8ec6979

--- dist/wikibase-vuejs-components.common.js
+++ dist/wikibase-vuejs-components.common.js
@@ -2452,7 +2452,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"1370e0b2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Sample.vue?vue&type=template&id=49bbfa45&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"5eee5069-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Sample.vue?vue&type=template&id=49bbfa45&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
 var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"sample"},[_c('h1',[_vm._v("Sample Header")])])}]
 
@@ -2727,7 +2727,7 @@ var component = normalizeComponent(
 )
 
 /* harmony default export */ var components_Sample = (component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"1370e0b2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Sample2.vue?vue&type=template&id=104b7346&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"5eee5069-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Sample2.vue?vue&type=template&id=104b7346&
 var Sample2vue_type_template_id_104b7346_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
 var Sample2vue_type_template_id_104b7346_staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"sample2"},[_c('h2',[_vm._v("Sampel 2 Header")])])}]
 
