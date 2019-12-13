61d5ffa94e3b9772ec77e6b9c711f09470ea3ae6
Jakob Warkotsch <j.warkotsch@gmail.com>
2019-12-13 13:35:48 +0100

    Remove dist/ directory from git
    Bug: T240329
    Change-Id: I648ff84812db1029090b25664b30470ba95f1dbd

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import ResizingTextField from './components/ResizingTextField.vue';
-import IndeterminateProgressBar from './components/IndeterminateProgressBar.vue';
-export { ResizingTextField, IndeterminateProgressBar, };
